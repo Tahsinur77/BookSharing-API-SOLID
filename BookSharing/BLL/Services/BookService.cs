3de0b7f82ff63523b9555885b720532675fb9f55
tahsinur <tahsinur777@gmail.com>
2022-04-13 03:01:38 +0600

    Book all done

---
+++ BookSharing/BLL/Services/BookService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using DAL;
+using DAL.Codefirst.Database;
+using ELL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class BookService
+    {
+        public static BookModel Get(int userId)
+        {
+            var book = DataAccessFactory.BookDataAccess().Get(userId);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Book, BookModel>());
+            var mapper = new Mapper(config);
+            var bookModel = mapper.Map<BookModel>(book);
+
+            return bookModel;
+        }
+        public static List<BookModel> Get()
+        {
+            var bookList = DataAccessFactory.BookDataAccess().Get();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Book, BookModel>());
+            var mapper = new Mapper(config);
+            var bookModelList = mapper.Map<List<BookModel>>(bookList);
+
+            return bookModelList;
+
+        }
+        public static bool Add(BookModel bookModel)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<BookModel, Book>());
+            var mapper = new Mapper(config);
+            var book = mapper.Map<Book>(bookModel);
+
+            var check = DataAccessFactory.BookDataAccess().Add(book);
+            return check;
+        }
+
+        public static bool Edit(BookModel bookModel)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<BookModel, Book>());
+            var mapper = new Mapper(config);
+            var book = mapper.Map<Book>(bookModel);
+
+            var flag = DataAccessFactory.BookDataAccess().Edit(book);
+            return flag;
+        }
+        public static bool Delete(int id)
+        {
+            var flag = DataAccessFactory.BookDataAccess().Delete(id);
+            return flag;
+        }
+    }
+}
---
+++ BookSharing/BookSharing/Controllers/BookController.cs
@@ -0,0 +1,67 @@
+﻿using BLL.Services;
+using ELL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookSharing.Controllers
+{
+    public class BookController : ApiController
+    {
+        [Route("api/Book/Add")]
+        [HttpPost]
+        public HttpResponseMessage Add(BookModel book)
+        {
+            if (ModelState.IsValid)
+            {
+                var flag = BookService.Add(book);
+
+                if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Added");
+                else return Request.CreateResponse(HttpStatusCode.OK, "Not Added");
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Book/list")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var list = BookService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+
+        [Route("api/Book/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            var list = BookService.Get(id);
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+
+        [Route("api/Book/Edit")]
+        [HttpPost]
+        public HttpResponseMessage Edit(BookModel book)
+        {
+            if (ModelState.IsValid)
+            {
+                var flag = BookService.Edit(book);
+                if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Edited");
+                else return Request.CreateResponse(HttpStatusCode.OK, "Not Edited");
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Book/Delete/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            var flag = BookService.Delete(id);
+            if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+            else return Request.CreateResponse(HttpStatusCode.OK, "Not Delete");
+        }
+
+    }
+}
