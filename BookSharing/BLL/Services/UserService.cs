ec0db750871a3ccfa74b149db4dc6b73532f06d8
tahsinur <tahsinur777@gmail.com>
2022-04-12 21:56:13 +0600

    Ell project added and UserService add in Bll

---
+++ BookSharing/BLL/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using DAL;
+using DAL.Codefirst.Database;
+using ELL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserService
+    {
+        public static UserModel Get(int userId)
+        {
+            var user = DataAccessFactory.UserDataAccess().Get(userId);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UserModel>());
+            var mapper = new Mapper(config);
+            var userModel = mapper.Map<UserModel>(user);
+
+            return userModel;
+        }
+        public static List<UserModel> Get()
+        {
+            var userList = DataAccessFactory.UserDataAccess().Get();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UserModel>());
+            var mapper = new Mapper(config);
+            var userModelList = mapper.Map<List<UserModel>>(userList);
+
+            return userModelList;
+
+        }
+        public static bool Add(UserModel userModel)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<UserModel, User>());
+            var mapper = new Mapper(config);
+            var user = mapper.Map<User>(userModel);
+
+            var check = DataAccessFactory.UserDataAccess().Add(user);
+            return check;
+        }
+
+        public static bool Edit(UserModel userModel)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<UserModel, User>());
+            var mapper = new Mapper(config);
+            var user = mapper.Map<User>(userModel);
+
+            var flag = DataAccessFactory.UserDataAccess().Edit(user);
+            return flag;
+        }
+        public static bool Delete(int id)
+        {
+            var flag = DataAccessFactory.UserDataAccess().Delete(id);
+            return flag;
+        }
+
+    }
+}
---
+++ BookSharing/ELL/Entites/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELL.Entites
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please fill your name")]
+        [StringLength(50, ErrorMessage = "Name must not excced 50")]
+        public string Name { get; set; }
+        [Required]
+        public string Role { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
