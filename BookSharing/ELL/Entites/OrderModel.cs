38084a05a7bd93ed1057aa6f929d784559158ca3
diponkorcs <diponkorchandro678@gmail.com>
2022-04-15 02:43:18 +0600

    Order Done

---
+++ BookSharing/ELL/Entites/OrderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELL.Entites
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Status { get; set; }
+    }
+}
